ec746acd81816e081f3795fea47e8134955550b9
Michael Madsen <mm@pug-works.dk>
2020-07-15 16:31:09 +0200

    added brainstorm text

---
+++ myScript.js
@@ -0,0 +1,18 @@
+/****** Brainstorm
+  Gyro Input test
+
+  A/B Test hvor man bruger gyro input til at ændre på en værdi. Det den ændrer på kan være en mindre markant ændring. Ændringen kan foregå på:
+    - Mouse clicks (Når man trykker på skærmen)
+    - Lys (Gøre det mørkere eller lysere)
+    - Lyd (i forhold til volume)
+
+    1. Det kan være at man skal udføre en opgave, f.eks. Et puslespil, sammenligning, udfylde et skema, og så ændrer den lille gyro på nogle værdier.
+    2. Man kan også måske tage en ekstern telefon (os som projekt personer der ændrer på værdier igennem testen.
+
+  Til data analyse kan man bruge både kval og kvant data. Eventuelt indsamle kvant data via koden. Data eksempler kan være:
+    1. Antal klik
+    2. Tid per side
+    3. I don’t know
+
+  Derudover kan man så have et interview efterfølgende hvor man så kan tale omkring der oplevelse, og så sammenligne resultater med kvant data’en. #MixedMethodology
+*/
