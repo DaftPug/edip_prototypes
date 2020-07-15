8a1ad38df1ba8d02f4bd0c91835068589cac7783
Michael Madsen <mm@pug-works.dk>
2020-07-15 16:33:15 +0200

    added brainstorm

---
+++ myScript.js
@@ -0,0 +1,10 @@
+/***** Brainstorm
+
+  Jeg synes det kunne være interessant at arbejde med data fra en API - f.eks. En Corona-Api, som giver nogle opdaterede værdier for hvor mange der er smittet i verden. Denne api(data derfra) kunne derefter påvirke kameraet, mikrofonen, Gyro inputtet etc - og man kunne dermed udarbejde nogle interessante interaktioner mellem daten og brugeren.
+
+  I tillæg til dette, kunne man også arbejde med lys og lyd i forhold til hvordan smitten spreder sig og fordeler sig rundt omkring i verden.
+
+  I forhold til dataanalysen synes jeg, at man skal anvende kvalitativ data - hvor man måler på hvordan brugerne oplever prototypen - forstår de f.eks idéen?, er det interessant? Er det en givende prototype? Hvad kan gøres bedre?
+
+*/
+
