d71e420d1b7b755440887ae15d2ef11139cbc799
Michael Madsen <mm@pug-works.dk>
2020-07-16 12:40:53 +0200

    created new hotncold branch

--- myScript.js
+++ myScript.js
@@ -1,10 +1,6 @@
 /***** Brainstorm
 
-  Multiplayer tron, hvor x spillere forbinder til en mobil og styrer deres figur ved hjælp af deres mobils gyroskop. Spillernes figurer kunne være forskellige former, som cirkler, trekanter, firkanter osv.
-
-  Når en spiller dør, så flasher spillerens skærm, så det er tydeligt, hvem der er død.
-
-  Baggrunden på skærmen kan også pulsere som et hjerteslag, hvor at hjertet, så pulser hurtigere jo mere bane der er lagt på skærmen og jo flere spillere der er røget ud
+  En prøver at finde en person via en skærm, som ændrer farve efter hvor tæt på personen er, en anden prøver at flygte, ved at mærke telefonens vibrationer, som indikerer, at personen kommer tættere på.
 
 */
 
